b72a8f46d54821c4a55f4b5b3e4fbdc75b0f8a2b
Phil Johnston <johnstonphilip@gmail.com>
2014-01-30 18:09:28 -0500

    beta1.0.0.3

--- mp-buttons.php
+++ mp-buttons.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Buttons
 Plugin URI: http://moveplugins.com
 Description: Insert buttons into TinyMCE
-Version: beta1.0.0.2
+Version: beta1.0.0.3
 Author: Move Plugins
 Author URI: http://moveplugins.com
 Text Domain: mp_buttons
